f0e4c63c9822d338373fb69766308a9fec91dd23
yuriy.dushenko <yuriy.dushenko@agropoisk.ru>
2022-09-20 15:18:39 +0300

    fix bugs

--- src/js/app.js
+++ src/js/app.js
@@ -15,19 +15,7 @@ const imgs = document.querySelectorAll('.ranking-img');
 const windowHeight = document.documentElement.clientHeight;
 const imgHeight = imgs[0].offsetHeight;
 imgs.forEach((item) => (item.style.top = windowHeight - item.offsetHeight + 'px'));
-// const imgWrap = document.querySelector('.img-wrap');
-// const overflowContainer = document.querySelector(".js-overflow-container");
 
-// overflowContainer.style.position = "sticky";
-// overflowContainer.style.height = imgs[0].offsetHeight + "px";
-// overflowContainer.style.top = windowHeight / 2 - imgs[0].offsetHeight / 2 + "px";
-
-
-
-// const topBlock = document.querySelector(".top-block");
-// const bottomBlock = document.querySelector(".bottom-block");
-
-// imgs.forEach( item => item.style.top = windowHeight / 2 - item.offsetHeight / 2 + "px" );
 
 if (html.clientWidth >= 1024) {
 
@@ -38,18 +26,13 @@ if (html.clientWidth >= 1024) {
 
 	function scrollHandler() {
     containers.forEach((container, index) => {
-      // const topCoord = container.getBoundingClientRect().top;
     
       const bottomCoord = container.getBoundingClientRect().top;
-      // const opacityValue = (topCoord + windowHeight - imgHeight) * 0.001;
       const opacityValue = (bottomCoord + imgs[0].offsetHeight) * 0.001;
     
       const block = container.querySelector('.ranking-img');
-      // const borderRadiusValue = parseFloat(block.style.borderRadius || 50);
-      // const borderRadiusValue = topCoord - (windowHeight - block.offsetHeight) * 1.3;
       if (index === containers.length - 1) return;
       block.style.opacity = opacityValue;
-      // block.style.borderRadius = borderRadiusValue <= 0 ? 0 : borderRadiusValue + "%";
       });
   }
 
--- src/js/components/burger-menu.js
+++ src/js/components/burger-menu.js
@@ -3,7 +3,6 @@ const NAV = document.querySelector('.js-burger');
 const BODY = document.querySelector('body');
 const CLASS_OVERFLOW = 'overflow';
 const CLASS_ACTIVE = 'active';
-const HEADER = document.querySelector('.header');
 const padding = window.innerWidth - BODY.clientWidth;
 
 const burgerMenu = (() => {
@@ -26,31 +25,33 @@ const burgerMenu = (() => {
 
     if (screen.width >= 1024) {
       BODY.style.paddingRight = `${padding}px`;
-      HEADER.style.paddingRight = `${padding}px`;
     }
   }
 
   const closeBurger = () => {
     BURGER.classList.remove(CLASS_ACTIVE);
     NAV.classList.remove(CLASS_ACTIVE);
-    BODY.classList.remove(CLASS_OVERFLOW);
-    BODY.style.paddingRight = 0;
-    HEADER.style.paddingRight = 0;
+
+    setTimeout(function () {
+      BODY.classList.remove(CLASS_OVERFLOW);
+      BODY.style.paddingRight = 0;
+    }, 300);
+
   }
 
 
-	$('a[href^="#"]').click(function () {
-		var href = $.attr(this, 'href')
+  $('a[href^="#"]').click(function () {
+    var href = $.attr(this, 'href')
 
-		closeBurger();
+    closeBurger();
 
-		$('html, body').animate({
-				scrollTop: $(href).offset().top,
-			},
-			500,
-		)
-		return false
-	});
+    $('html, body').animate({
+        scrollTop: $(href).offset().top,
+      },
+      500,
+    )
+    return false
+  });
 
   const init = () => {
     burgeInit();
